87e2d8ccabb8e8a19cf7bd0a43466052bf4ebcdf
ZeyadOsama <zeyad.osama@gmail.com>
2019-10-12 17:05:50 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,14 @@
+//
+// Created by Zeyad Osama on 2019-10-12.
+//
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    return EXIT_SUCCESS;
+}
